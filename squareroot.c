ceb75465ac4c52e999e2f0292ab4f895fbbbfb6a
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-06-22 17:50:29 +0530

    square root.c

---
+++ squareroot.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<math.h>
+
+float main() {
+    
+        float t;
+        printf("Enter a number:");
+        scanf("%f",&t);
+        
+        printf("square root = %f",sqrt(t));
+        
+        
+    
+    return 0;
+}
