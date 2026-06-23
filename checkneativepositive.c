2e9bd81ced0f272e8bd835006442bc5a2604df7b
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-06-23 15:16:35 +0530

    checking number is positive or negative

---
+++ checkneativepositive.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int x;
+    printf("enter  value:");
+    scanf("%d",&x);
+    if (x>0)
+ {
+    printf(" number is positive:");
+    }
+    else 
+    {
+        printf(" number is negative:");    
+    }
+}
