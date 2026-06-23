8d3fd52b9061b201b3e7b5f54ab6e7f07f4f5f19
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-06-23 15:23:07 +0530

    checking salary and bonus

---
+++ checksalarybonus.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int salary;
+    printf("enter  salary:");
+    scanf("%d",&salary);
+    if (salary>5000)
+ {
+    printf(" bonus is 1000");
+    }
+    else 
+    {
+        printf(" bonus is 500");    
+    }
+}
