4a9d9254ae52d377601df48c35fc9b6e4ab6f307
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-06-23 15:19:21 +0530

    student is pass or fail

---
+++ checkpassfail.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int x;
+    printf("enter  total percentage:");
+    scanf("%d",&x);
+    if (x>=40)
+ {
+    printf(" student is pass:");
+    }
+    else 
+    {
+        printf(" student is fail:");    
+    }
