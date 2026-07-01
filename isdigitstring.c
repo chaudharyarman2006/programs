79b3776e99216c4a47b9413cf6ae2a6eb8223a65
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-01 17:41:12 +0530

    checking digits in string

---
+++ isdigitstring.c
@@ -0,0 +1,28 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+int main() {
+    char ch[20];
+    printf("enter password:");
+    scanf("%s",ch);
+    int  l=strlen(ch);
+    int d=0;
+    for(int i=0;i<l;i++)
+    {
+    if(isdigit(ch[i]))
+    { 
+        d++;
+    }
+    
+    }
+    if (d>0)
+    { 
+        printf("password accepted");
+    }
+    else
+    {
+        printf("password should have atleast 1 digit");
+    }
+    
+}
