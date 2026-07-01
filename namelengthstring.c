5c7824ee864f0590c4561a2d1764e7d3c9f0277a
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-01 14:49:37 +0530

    input a string and print its length

---
+++ namelengthstring.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<string.h>
+int main() {
+    char ch[20];
+    printf("enter name:");
+    scanf("%s",ch);
+    int  l=strlen(ch);
+    printf("length=%d",l);
+}
