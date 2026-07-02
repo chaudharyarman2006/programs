7ffcbd67c1efe9c4e2a2b1d0f250edcca27b9922
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-02 10:34:12 +0530

    input an array and print its reverse

---
+++ reversearray.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    for(int i=0;i<5;i++) {
+        printf("\nenter element number %d:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(int i=4;i>=0;i--) {
+        printf("%d ",arr[i]);
+}
+    return 0;
+}
