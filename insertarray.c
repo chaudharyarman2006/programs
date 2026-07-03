f3f62841ec04f027053fa9e5d46fe527d1a6d158
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-03 14:43:29 +0530

    Insert an element at a given position

---
+++ insertarray.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int ar[7]={234,54,3,45,4,35,3};
+    
+    for(int i=0;i<7;i++)
+{
+     ar[2]=69;
+
+}
+for(int i=0;i<7;i++){
+    printf("%d\n",ar[i]);
+}
+    return 0;
+}
