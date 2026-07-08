486946124eec48996c4d9b36f3e2c874e94434b4
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-08 12:55:59 +0530

    input a matrix and transpose it

---
+++ transposematrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a[3][3];
+    int t[3][3];
+int i,j;
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+            
+        }
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            t[j][i]=a[i][j];
+            
+        }
+    }
+    for(i=0;i<3;i++){
+         for(j=0;j<3;j++){
+             printf(" %d",t[i][j]);
+         }
+         printf("\n");
+    }
+    return 0;
+}
