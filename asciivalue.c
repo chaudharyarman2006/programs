0666f2c2c63c28c5c38b241c38c450a4ebd30c55
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-09 06:48:23 +0530

    input a character and print its ascii value

---
+++ asciivalue.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    char ch='3';
+    int x=(int)ch;
+    printf("%d",x);
+
+    return 0;
+}
