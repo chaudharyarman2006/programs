0b6598e4e0707e641968793c446cb714f3b66230
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-10 16:30:02 +0530

    input two matrices and add them

---
+++ addtwomatrix.c
@@ -0,0 +1,32 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a[3][3],b[3][3],c[3][3],i,j;
+    printf("enter matrix a:\n");
+    for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                scanf("%d",&a[i][j]);
+            }
+    }
+        printf("enter matrix b:\n");
+        for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                scanf("%d",&b[i][j]);
+            }
+    }
+    for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                c[i][j]=a[i][j]+b[i][j];
+            }
+    }
+    printf("addition of two matrices are:\n");
+    for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                printf(" %d",c[i][j]);
+            }
+            printf("\n");
+    }
+
+    return 0;
+}
