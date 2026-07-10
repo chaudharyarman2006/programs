54c3c6445d79fdab0f6bbcb1ff1db8f78bd30adc
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-10 16:56:34 +0530

    input a matrix and print sum of its diagonal elements (00,11,22)

---
+++ sumdiagonalmatrix.c
@@ -0,0 +1,30 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a[3][3],i,j;
+    printf("enter matrix a:\n");
+    for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                scanf("%d",&a[i][j]);
+            }
+    }
+int sum=0;
+    for (i=0;i<3;i++){
+            for (j=0;j<3;j++){
+                
+                    if(i==j)
+                    sum=sum+a[i][j];
+            }
+    }
+    printf("sum of diagonal element is:%d",sum);
+    // printf("multiplication' of two matrices are:\n");
+    // for (i=0;i<3;i++){
+    //         for (j=0;j<3;j++){
+    //             printf(" %d",c[i][j]);
+    //         }
+    //         printf("\n");
+    // }
+
+    return 0;
+}
