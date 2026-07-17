1e693bdf7ca040cdcc6a6875b727901a1d0a8f90
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-17 22:23:17 +0530

    Print all odd numbers between 1 to 50

---
+++ oddnumberusingloop.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("odd numbers>>>>>>\n");
+    for(int i=1;i<51;i=i+2)
+    {
+        printf(" %d\n",i);
+    }
+
+    return 0;
+}
