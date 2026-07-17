f813cb165565f5611fffd3e3139adb545257bd48
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-17 22:20:29 +0530

    Print numbers from 10 to 1 using a loop

---
+++ print10to1usingloop.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    for(int i=10;i>0;i--)
+    {
+        printf(" %d\n",i);
+    }
+
+    return 0;
+}
