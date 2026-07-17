7babc64ce71d99362bf293c28aa6dc7d09ec10b8
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-17 22:24:53 +0530

    Print all even numbers between 1 to 50

---
+++ evennumberusingloop.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    printf("even numbers>>>>>>\n");
+    for(int i=2;i<51;i=i+2)
+    {
+        printf(" %d\n",i);
+    }
+
+    return 0;
+}
