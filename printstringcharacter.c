7d041230ba975bb1969d5e2beecf7226b90fd853
chaudharyarman2006 <chaudharyarman2006@gmail.com>
2026-07-20 14:36:39 +0530

    Print all characters of a string using a loop

---
+++ printstringcharacter.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[]="arman chaudhary";
+    int l=strlen(str);
+    for(int i =0;i<l;i++)
+    {
+        printf(" %c\n",str[i]);
+    }
+
+    return 0;
+}
